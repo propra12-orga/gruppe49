b7fb42fe2b9b6cf84071b5c27908ad8f66d9f662
Atanur Oeztuer <atanur.oeztuerk@uni-duesseldorf.de>
2012-06-12 01:05:32 +0200

    Bomen animation hinzugefuegt

--- src/SetBombThread.java
+++ src/SetBombThread.java
@@ -5,7 +5,7 @@ public class SetBombThread implements Runnable {
 	private int x,y;
 	private boolean explode=true; 
 	private int spieler;
-
+	private StoneEntity st;
 	
 	public SetBombThread(int spieler,int x,int y){
 		this.spieler=spieler;
@@ -28,11 +28,51 @@ public class SetBombThread implements Runnable {
 					EntityManager.entitylistb1.add(bomb);
 					Thread.sleep(3000);
 					EntityManager.entitylistb1.remove(bomb);
+					
+					for(int i=1;i<3;i++){
+						
+						StoneEntity	st1 =new StoneEntity("explosion.png",x+50*i,y);
+							EntityManager.entitylistb1.add(st1);
+							StoneEntity	st2 =new StoneEntity("explosion.png",x-50*i,y);
+							EntityManager.entitylistb1.add(st2);
+							StoneEntity	st3 =new StoneEntity("explosion.png",x,y+50*i);
+							EntityManager.entitylistb1.add(st3);
+							StoneEntity	st4 =new StoneEntity("explosion.png",x,y-50*i);
+							EntityManager.entitylistb1.add(st4);
+							Thread.sleep(500);
+							EntityManager.entitylistb1.remove(st1);
+							EntityManager.entitylistb1.remove(st2);
+							EntityManager.entitylistb1.remove(st3);
+							EntityManager.entitylistb1.remove(st4);
+						
+					}
+
 				}
 				if(spieler==1){
 					EntityManager.entitylistb2.add(bomb);
 					Thread.sleep(3000);
 					EntityManager.entitylistb2.remove(bomb);
+					
+					for(int i=1;i<3;i++){
+						
+						StoneEntity	st1 =new StoneEntity("explosion.png",x+50*i,y);
+							EntityManager.entitylistb2.add(st1);
+							StoneEntity	st2 =new StoneEntity("explosion.png",x-50*i,y);
+							EntityManager.entitylistb2.add(st2);
+							StoneEntity	st3 =new StoneEntity("explosion.png",x,y+50*i);
+							EntityManager.entitylistb2.add(st3);
+							StoneEntity	st4 =new StoneEntity("explosion.png",x,y-50*i);
+							EntityManager.entitylistb2.add(st4);
+							Thread.sleep(500);
+							EntityManager.entitylistb2.remove(st1);
+							EntityManager.entitylistb2.remove(st2);
+							EntityManager.entitylistb2.remove(st3);
+							EntityManager.entitylistb2.remove(st4);
+						
+					}
+					
+					
+					
 				}
 
 				explode=false;
