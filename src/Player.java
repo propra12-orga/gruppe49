7e0dfe3e0dcc8fcd0f69134b10de66849611abae
Atanur Oeztuer <atanur.oeztuerk@uni-duesseldorf.de>
2012-06-25 17:12:44 +0200

    refactor

---
+++ src/Player.java
@@ -0,0 +1,19 @@
+
+
+public class Player {
+	private String name;
+	private InputHandler input;
+	
+	public Player(String name,InputHandler input){
+		this.name=name;
+		this.input=input;
+	}
+	
+	public InputHandler getInputHandler(){
+		return input;
+	}
+	
+	public String getName(){
+		return name;
+	}
+}
