84bd591b3a2c1250693976bc4a4f82219c0ec891
Atanur Oeztuer <atanur.oeztuerk@uni-duesseldorf.de>
2012-07-02 17:12:05 +0200

    Zuerueck auf meilen stein 1 und 2

---
+++ src/ReadFile.java
@@ -0,0 +1,54 @@
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+
+public class ReadFile {
+
+	private File myFile;
+	private BufferedReader br;
+	private int [][]a;
+	private int n,m;
+	
+	public ReadFile(String fileName){
+		myFile =new File(fileName);
+	}
+	
+	public int[][] getContent(){
+		String buffer="";
+		String line="";
+		int counter=0;
+		n=0;
+		try {
+			br=new BufferedReader(new InputStreamReader(new FileInputStream(myFile)));
+			while((line=br.readLine())!=null){
+				buffer+=line;
+				n++;
+			}
+			m=buffer.length()/n;
+			a= new int[n][m];
+			
+			for(int i=0;i<n;i++){
+				for(int j=0;j<m;j++){
+					a[i][j]=Character.getNumericValue(buffer.charAt(counter));
+					counter++;
+				}
+			}
+			br.close();
+			
+		} catch (Exception e) {
+				System.out.println("Datei koonte nicht gefunden werden");
+		}
+		return a;
+	}
+	
+	public int getDimX(){
+		return n;
+	}
+	public int getDimY(){
+		return m;
+	}
+	
+}
---
+++ src/SpriteManager.java
@@ -0,0 +1,22 @@
+
+import java.awt.Image;
+import java.util.HashMap;
+
+import javax.swing.ImageIcon;
+
+
+public class SpriteManager {
+
+	public static SpriteManager manager=new SpriteManager();
+	private HashMap<String, Sprite> map=new HashMap<String,Sprite>();
+	
+	public Sprite getSprite(String src){
+		if(map.get(src)!=null)return map.get(src);
+		ImageIcon icon = new ImageIcon(getClass().getResource(src));
+		Image img=icon.getImage();
+		Sprite s=new Sprite(img);
+		map.put(src, s);
+		return s;
+	}
+}
+ 
